b14852713dea055d3ed6c60289fb130f8fdde27c
amageton23 <111858381+stevethefourth@users.noreply.github.com>
2022-08-29 02:16:16 +1000

    feature-hide complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,20 +6,29 @@ public class PrintAndHide: MonoBehaviour
 {
 
     public Renderer rend;
-    private int start = 3;
+    private int i = 3;
+    private int randomNumber;
     
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        int rnd = Random.Range(200, 251);
+        randomNumber = rnd;
     }
 
     // Update is called once per frame
     void Update()
     {
-        start += 1;
-        Debug.Log($"{gameObject.name}" + ":" + $"{start}");
-
+        i += 1;
+        Debug.Log($"{gameObject.name}" + ":" + $"{i}");
+        if (gameObject.tag == "Red" && i == 100) 
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i == randomNumber)
+        {
+            rend.enabled = false;
+        }
     }
 }
