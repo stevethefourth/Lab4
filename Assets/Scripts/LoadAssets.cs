86c12e2db649872791c64f9e29b9c564388c1da4
amageton23 <91231816+amageton23@users.noreply.github.com>
2022-08-29 01:02:15 +1000

    Pass 60 complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,6 +6,7 @@ public class LoadAssets : MonoBehaviour
 {
 
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
